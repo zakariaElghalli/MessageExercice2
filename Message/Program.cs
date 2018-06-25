2069383f47e3a4585a8d998a230017b176486258
zakariaElghalli <40542598+zakariaElghalli@users.noreply.github.com>
2018-06-25 04:06:23 +0200

    Ajoutez des fichiers projet.

---
+++ Message/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Message
+{
+    public abstract class Message
+    {
+        public abstract void execute();
+    }
+
+    public class MessageA : Message
+    {
+
+
+
+
+
+        public override void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MessageB : Message
+    {
+
+
+
+
+
+        public override void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MessageC : Message
+    {
+
+
+
+
+        public override void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new MessageA();
+            message.execute();
+        }
+    }
+}
